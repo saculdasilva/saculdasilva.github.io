18923dcc6485309cda46c865f6c7fc319a3c0266
Lucas Silva <saculdasilva@hotmail.com>
2024-04-09 13:25:41 +0100

    chore: quick homepage content update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Lucas Silva';
+export const SITE_DESCRIPTION = "Lucas Silva: A Portuguese Front-End Developer (and sometimes Full-Stack) from the enchanting city of Porto, with a passion for bleeding-edge tech.";
